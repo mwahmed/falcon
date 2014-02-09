de586b7ce7546a0b8ea282a8511532813d6d2f6c
Ayush Kulkarni <ayush.kulkarni1991@gmail.com>
2014-02-08 21:32:09 -0500

    Adding zwords, anti-zwords, top 10 words

--- summarizer/cuewords.py
+++ summarizer/cuewords.py
@@ -3,12 +3,12 @@ import os
 import pdb
 from collections import defaultdict
 
-cuewordDS = defaultdict(int)
+cueword_zword_DS = defaultdict(float)
 
 # TODO: Should we get all the cuewords, i.e. from AMI and the other corpus in Murray's thesis?
 # load the cuewords into memory from the file
 def populate_cueword_DS(cuewordsFileName):
-	global cuewordDS
+	global cueword_zword_DS
 	cuewordsFile = open(cuewordsFileName)
 	cuewordlines = cuewordsFile.readlines()
 	for line in cuewordlines:
@@ -16,8 +16,8 @@ def populate_cueword_DS(cuewordsFileName):
 		word = wordList[0]
 		# cuewords are ranked from 1 - 70.
 		# Normalize the cuewords, so that the highest cueword has a rank of 1.0
-		score = (70 - float(wordList[1].strip()))/69.0
-		cuewordDS[word] = score
+		score = (71 - float(wordList[1].strip()))/70.0
+		cueword_zword_DS[word] = score
 		
 # read each sentence and calculate its score
 def parse_speech_transcript_and_get_summary(transcriptLines):
@@ -28,31 +28,60 @@ def parse_speech_transcript_and_get_summary(transcriptLines):
 
 	return importantSentences
 		
-# Calculate the score of a sentence, based on the cueword score of each sentence
+# Calculate the score of a sentence, based on its cueword score
 def calculate_sentence_score(sentence):
 	# To make lookup fast, see if the word is a key in the dictionary
-	global cuewordDS
+	global cueword_zword_DS
 	sentence = sentence.lower()
 	score = 0
 	sentenceList = sentence.split(" ")
 	for word in sentenceList:		
 		# cuewords scores range from 1 - 70, with decreasing importance
-		score += cuewordDS[word]
+		score += cueword_zword_DS[word]
 	
 	return score
 
 
 # speechTranscriptionLines is the transcript, stored as a list of strings
-def get_cuewords_scores(speechTranscriptionLines):
-	populate_cueword_DS("/Users/ayushkulkarni/Documents/4S/ECE496/falcon/summarizer/cuewords.txt")
+def get_cuewords_zwords_scores(speechTranscriptionLines, zwords, inv_zwords):
+	#populate_cueword_DS("/Users/ayushkulkarni/Documents/4S/ECE496/falcon/summarizer/cuewords.txt")
+	populate_cueword_DS("/home/ubuntu/falcon/summarizer/cuewords.txt")
+	if zwords != "":
+		get_zwords_scores(zwords)
+	
+	if inv_zwords != "":
+		handle_inv_zwords(inv_zwords)
+
 	summary_idxs = parse_speech_transcript_and_get_summary(speechTranscriptionLines)
 	return summary_idxs
 
-# speechTranscriptionLines is same as above
 # zwords is a string containing user defined keywords separated by a ";", all in lowercase
 # The above function should call this function before populate_cueword_DS
 def get_zwords_scores(zwords):
-	global cuewordDS
-	zwords_list = zwords.split(";")
+	global cueword_zword_DS
+	zwords_list = list()
+	if zwords.find(";"):
+		zwords_list = zwords.split(";")
+	else:
+		# if there is only a single zword
+		zwords_list.append(zwords)
 	for zword in zwords_list:
-		cuewordDS[zword] = 1.0
+		# These will also be added to the cuewordsDS
+		cueword_zword_DS[zword] = 1.0
+
+
+# inv_zwords are words that the user does not want to include in the summary,
+# they are passed in as a string separated by ";"
+
+# This function assigns a score of negative 1 to inv_zwords so that they do not get included in the summary
+def handle_inv_zwords(inv_zwords):
+	global cueword_zword_DS
+	inv_zwords_list = list()
+	if inv_zwords.find(";"):
+		inv_zwords_list = inv_zwords.split(";")
+	else:
+		# if there's only a single inv_zword
+		inv_words_list.append(inv_zwords)	
+	for inv_zword in inv_zwords_list:
+		# Add it to the cuewordsDS with a score of -1
+		cueword_zword_DS[inv_zword] = -1.0 
--- summarizer/summarizer.py
+++ summarizer/summarizer.py
@@ -5,22 +5,33 @@ import sys
 from bson.objectid import ObjectId
 from pymongo import MongoClient
 
-# Take the transcription and summary size ( as % of total sentences ) as input,
-# call the tfidf module and the cuewords module passing in the respective limits to each 
-# The results should also return the sentence idx, so that the sentences can be displayed in order.
 
-# transcriptionLines stores the transcription as a list of strings
-# summary_limit is the percentage of lines that should be in the summary
-# saved_df_file_path is the path to the file containing df values for words
-def summarize(transcriptionLines, summary_limit_percent, saved_df_file_path):
+# Global variable used for debugging
+debug_sentence_score = list()
+
+'''
+Take the transcription and summary size ( as % of total sentences ) as input,
+call the tfidf module and the cuewords module passing in the respective limits to each 
+The results should also return the sentence idx, so that the sentences can be displayed in order.
+
+ transcriptionLines stores the transcription as a list of strings
+ summary_limit is the percentage of lines that should be in the summary
+ saved_df_file_path is the path to the file containing df values for words
+ zwords is string containing user defined keywords, separated by a ";"
+ inv_zwords is same as above, but these are words NOT to be matched
+'''
+
+def summarize(transcriptionLines, summary_limit_percent, saved_df_file_path, zwords, inv_zwords):
 	linecount = len(transcriptionLines)
 	summary_limit_abs = int(summary_limit_percent/100.0 * linecount)
-	cueword_sentence_scores_idxs = cuewords.get_cuewords_scores(transcriptionLines)
+	# Note that cuewords and zwords are calculated together, so this function returns the 
+	# cueword and the zword scores
+	cueword_zword_sentence_scores_idxs = cuewords.get_cuewords_zwords_scores(transcriptionLines, zwords, inv_zwords)
 	tfidf_sentence_scores_idxs = tfidf.get_tfidf_scores(transcriptionLines, saved_df_file_path)
 	combined_summary_scores = list()
 	for idx in xrange(linecount):
 		try:
-			combined_score = tfidf_sentence_scores_idxs[idx][0] + cueword_sentence_scores_idxs[idx][0]
+			combined_score = tfidf_sentence_scores_idxs[idx][0] + cueword_zword_sentence_scores_idxs[idx][0]
 			combined_summary_scores.append((combined_score, idx))
 		except:
 			print "#### Exception ####"
@@ -34,10 +45,18 @@ def summarize(transcriptionLines, summary_limit_percent, saved_df_file_path):
 	for score, idx in combined_summary_scores[:summary_limit_abs]:
 		summary_idxs.append(idx)
 
+	# This for loop is for debugging purposes
+	for score, idx in combined_summary_scores:
+		debug_sentence_score.append((transcriptionLines[idx], score))
 	summary_idxs.sort()
 	summary = [transcriptionLines[idx] for idx in summary_idxs]
+	
 	return summary
 
+def debug_summarizer():
+	global debug_sentence_score
+	for line in debug_sentence_score:
+		print line
 
 # Get the transcription from the database
 # Convert it into a list of strings
@@ -49,7 +68,7 @@ def main(post_id, summary_limit_percent):
 	transcriptionText = document['text']
 	transcriptionLines = transcriptionText.split("\n")
 	summary_limit_percent = float(summary_limit_percent)
-	return summarize(transcriptionLines, summary_limit_percent, "/Users/ayushkulkarni/Documents/4S/ECE496/falcon/summarizer/saved_df.scores")
+	return summarize(transcriptionLines, summary_limit_percent, "/home/ubuntu/falcon/summarizer/saved_df.scores")
 
 
 if __name__ == "__main__":
@@ -58,3 +77,10 @@ if __name__ == "__main__":
 	summary =  main(post_id, summary_limit_percent)
 	for line in summary:
 		print line
+
+	# delimiter between output
+	print ";;;ayush;;;"
+	# 	
+	top10words = tfidf.get_top10_words()
+	for word in top10words:
+		print word
